119c803967ea2a31f0bb013c4557918e2acf8afb
Helena Pedro <mbeua94@gmail.com>
2026-02-27 14:10:30 -0600

    lab5

--- springintegration/EnterpriseServiceBus/src/main/java/esb/Order.java
+++ springintegration/EnterpriseServiceBus/src/main/java/esb/Order.java
@@ -1,33 +1,36 @@
 package esb;
 
 public class Order {
-	private String orderNumber;
-	private double amount;
+    private String orderNumber;
+    private double amount;
 
-	public Order(String orderNumber, double amount) {
-		super();
-		this.orderNumber = orderNumber;
-		this.amount = amount;
-	}
+    public Order() {
+    }
 
-	public String getOrderNumber() {
-		return orderNumber;
-	}
+    public Order(String orderNumber, double amount) {
+        super();
+        this.orderNumber = orderNumber;
+        this.amount = amount;
+    }
 
-	public void setOrderNumber(String orderNumber) {
-		this.orderNumber = orderNumber;
-	}
+    public String getOrderNumber() {
+        return orderNumber;
+    }
 
-	public double getAmount() {
-		return amount;
-	}
+    public void setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+    }
 
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
-	
-	public String toString(){
-		return "order: nr="+orderNumber+" amount="+amount;
-	}
+    public double getAmount() {
+        return amount;
+    }
 
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "order: nr=" + orderNumber + " amount=" + amount;
+    }
 }
